19577be7dda250a96691bf9d1e345c9ea6f12302
mariia <rmashka2001@gmail.com>
2021-11-05 19:48:31 +0200

    1.2

--- lesson1/task2/src/userData-context..js
+++ lesson1/task2/src/userData-context..js
@@ -1,8 +1,3 @@
 import React from 'react';
 
-export const userData = {
-    anotherUser: { name: 'not a Nikola Tesla', avatar_url: 'some Url' },
-    teslaUser: { name: 'Nikola Tesla', avatar_url: 'https://avatars3.githubusercontent.com/u10001' },
-};
-
-export const userDataContext = React.createContext(userData.teslaUser);
+export const DataContext = React.createContext();
