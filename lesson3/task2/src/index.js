e3caa268fd9196f74703654a6540d1897331a90d
mariia <rmashka2001@gmail.com>
2021-11-13 20:33:46 +0200

    3.2

--- lesson3/task2/src/index.js
+++ lesson3/task2/src/index.js
@@ -1,30 +1,8 @@
-import './index.scss';
-import store, { increment, decrement, reset } from './store.js';
+import { store, increment, decrement } from './counter';
 
-const resultElem = document.querySelector('.counter__result');
-const decrementBtn = document.querySelector('[data-action = "decrement"]');
-const incrementBtn = document.querySelector('[data-action="increment"]');
-const resetBtn = document.querySelector('[data-action="reset"]');
+store.subscribe(() => console.log(store.getState()));
 
-const onIncrement = () => {
-    store.dispatch(increment());
-};
-
-const onDecrement = () => {
-    store.dispatch(decrement());
-};
-
-const onReset = () => {
-    store.dispatch(reset());
-};
-
-incrementBtn.addEventListener('click', onIncrement);
-decrementBtn.addEventListener('click', onDecrement);
-resetBtn.addEventListener('click', onReset);
-
-store.subscribe(() => {
-    const state = store.getState();
-    const currentValue = state.history.reduce((acc, value) => acc + +value, 0);
-    const historyString = state.history.join('');
-    resultElem.textContent = state.history.length === 0 ? '' : `${historyString} = ${currentValue}`;
-});
+store.dispatch(increment());
+store.dispatch(increment());
+store.dispatch(increment());
+store.dispatch(decrement());
