b1e09f82bbad973dfb8393ed31a861df7fcf4591
mariia <rmashka2001@gmail.com>
2021-11-30 09:55:44 +0200

    7.1

---
+++ lesson7/task1/src/tasks/tasks.selectors.js
@@ -0,0 +1,7 @@
+import { createSelector } from 'reselect';
+
+export const tasksListSelector = state => state.tasks.tasksList;
+
+export const sortedTasksListSelector = createSelector([tasksListSelector], tasksList =>
+    tasksList.slice().sort((a, b) => a.done - b.done),
+);
